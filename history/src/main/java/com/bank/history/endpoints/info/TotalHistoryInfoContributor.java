cfb044744b1dadb805997bd0a1152bd383aa0fef
Artem2605 <artyom260599@mail.ru>
2023-12-30 20:30:00 +0300

    обогатил актуатор информацией в history [bk-34]

---
+++ history/src/main/java/com/bank/history/endpoints/info/TotalHistoryInfoContributor.java
@@ -0,0 +1,28 @@
+package com.bank.history.endpoints.info;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.info.Info;
+import org.springframework.boot.actuate.info.InfoContributor;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+import java.time.LocalTime;
+
+@Component
+public class TotalHistoryInfoContributor implements InfoContributor {
+
+    private Environment environment;
+
+    @Autowired
+    public TotalHistoryInfoContributor(Environment environment) {
+        this.environment = environment;
+    }
+
+    @Override
+    public void contribute(Info.Builder builder) {
+        builder.withDetail("Название", environment.getProperty("spring.application.name"));
+        builder.withDetail("ArtifactId", environment.getProperty("spring.application.artifactId"));
+        builder.withDetail("Время запуска", LocalTime.now());
+        builder.withDetail("Версия приложения", environment.getProperty("spring.application.version"));
+        builder.withDetail("Context-path", environment.getProperty("server.servlet.context-path"));
+    }
+}
